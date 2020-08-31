7bd9b3da1cb4b6482ea32d8af24818e7755bc5f8
BobKitty <mrbobkitty@mail.ru>
2020-09-01 00:28:18 +0700

    Hello React

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -6,7 +6,7 @@ function HeaderComponent() {
     console.log(styles);
     return (
         <header>
-            <h1 className={styles.example}>Test</h1>
+            <h1 className={styles.example}>Hello React</h1>
         </header>
     );
 }
